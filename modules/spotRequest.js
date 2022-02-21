e4315f26dbf44a97ca90c0fec5c82f8a30b84952
csc530 <cscousin@lakeheadu.ca>
2022-02-21 11:43:58 -0500

    perf(modules): :zap: Improve structere of Spot request
    I simplified how the signature is hashed and the exports of the module
    
    The toString method will not work if there are values in the params property

--- modules/spotRequest.js
+++ modules/spotRequest.js
@@ -1,68 +1,45 @@
 const crypto = require('crypto-js');
-const { isArray, isObject, objectToString } = require('./objToString');
-
-const signRequest =
-	/**
-	 * From https://exchange-docs.crypto.com/spot/index.html#digital-signature
-	 * Generate digital signature for a request using API key secret
-	 * @param {SpotRequest} request_body full API request's body
-	 * @param {String} api_key public API key
-	 * @param {String} secret secret API key
-	 * @returns {String} the request's body the digital signature = request body's hex value encoded with API secret
-	 */
-	(request_body, api_key, secret) => {
-		const { id, method, params, time, apiKey } = request_body;
-		const paramsString = objectToString(params);
-		/**request url + params*/
-		const sigPayload = method + id + apiKey + paramsString + time;
-		/**encode request string with crypto[graphic] key's hex value**/
-		request_body.sig = crypto.HmacSHA256(sigPayload, secret).toString(crypto.enc.Hex);
-		return request_body.sig;
-	};
+const { objectToString } = require('./objToString');
 
 /**
  * A request to Crypto.com exchange API
  */
 class SpotRequest {
-	/**method to be invoked*/
+	/**{String} method to be invoked*/
 	method = '';
-	/**request identifier*/
+	/**{Number} request identifier*/
 	id = 0;
-	/**public API key*/
-	apiKey = '';
-	/**parameters for the method*/
+	/**{String} public API key*/
+	api_key = '';
+	/**{Object} parameters for the method*/
 	params = {};
-	/**Current timestamp in milliseconds since the Unix epoch*/
-	time = -1;
-	/**the digital signature for this API request*/
+	/**{Number} Current timestamp in milliseconds since the Unix epoch*/
+	nonce = -1;
+	/**{String} the digital signature for this API request*/
 	sig = '';
 	/**
 	 * Constructor to return a new SpotRequest
 	 * @param {String} method method to be invoked
-	 * @param {Number} id request identifier
-	 * @param {String} apiKey public API key
-	 * @param {Object} params parameters for the method
+	 * @param {Number?} id request identifier
+	 * @param {String?} apiKey public API key
+	 * @param {Object?} params parameters for the method
 	 * @param {Number} time Current timestamp in milliseconds since the Unix epoch
-	 * @param {String} apiSecret private/sercret API key
+	 * @param {String?} apiSecret private/secret API key
 	 */
 	constructor(id, method, apiKey, params, time, apiSecret) {
-		this.id = this.method = this.apiKey = this.params = this.time = this.sig = null;
-		if (!!method && typeof method === 'string' && !!time && typeof time === 'number') {
-			this.id = id;
-			this.method = method;
-			this.apiKey = apiKey;
-			this.params = params;
-			this.time = time;
-			this.sig = signRequest(this, apiKey, apiSecret);
-		} else {
-			console.error('Method ' + method + '\ntime ' + time + '');
-		}
+		this.id = id;
+		this.method = method;
+		this.api_key = apiKey;
+		this.params = params;
+		this.nonce = time;
+		if (apiSecret)
+			this.generateSignature(apiSecret);
 	}
 	/**
 	 * configure each property of the request
 	 * @param {String} method method to be invoked
 	 * @param {Number} id request identifier
-	 * @param {String} apiKey public API key
+	 * @param {String} api_key public API key
 	 * @param {Object} params parameters for the method
 	 * @param {Number} time Current timestamp in milliseconds since the Unix epoch
 	 * @param {String} sig the signature for the API request
@@ -70,9 +47,9 @@ class SpotRequest {
 	config(id, method, apiKey, params, time, sig) {
 		this.id = id;
 		this.method = method;
-		this.apiKey = apiKey;
+		this.api_key = apiKey;
 		this.params = params;
-		this.time = time;
+		this.nonce = time;
 		this.sig = sig;
 	}
 	/**
@@ -81,7 +58,17 @@ class SpotRequest {
 	 * @returns {String} digital signature for this request
 	 */
 	generateSignature(apiSecret) {
-		this.sig = signRequest(this, this.apiKey, apiSecret);
+		const paramsString = objectToString(this.params);
+		/**request url + params*/
+		//only add none null/empty params
+		let sigPayload = '';
+		sigPayload += this.method ? this.method : '';
+		sigPayload += this.id ? this.id : '';
+		sigPayload += this.api_key ? this.api_key : '';
+		sigPayload += paramsString ? paramsString : '';
+		sigPayload += this.nonce ? this.nonce : '';
+		/**encode request string with crypto[graphic] key's hex value**/
+		this.sig = crypto.HmacSHA256(sigPayload, apiSecret).toString(crypto.enc.Hex);
 		return this.sig;
 	}
 	/**
@@ -96,9 +83,36 @@ class SpotRequest {
 		url += 'id=' + this.id + and;
 		url += 'method=' + this.method + and;
 		url += 'params=' + objectToString(this.params) + and;
-		url += 'api_key=' + this.apiKey + and;
-		url += 'nonce=' + this.time;
+		url += 'api_key=' + this.api_key + and;
+		url += 'nonce=' + this.nonce;
 		return url;
+	}
+	/**
+	 * A JSON representation of the request
+	 * @returns {String} a string representation of this API request
+	 */
+	toString() {
+		const keys = Object.keys(this);
+		const values = keys.map(key => this[key]);
+		let string = '{';
+		for (let i = 0; i < keys.length; i++) {
+			if (keys[i] === 'params') string += `"${keys[i]}":{` + objectToString(values[i]) + '}';
+			/*{
+				for (const param in values[i]) {
+					if (Object.hasOwnProperty.call(values[i], param)) {
+						const element = values[i][param];
+						
+					}
+				}
+			}*/
+			else if (typeof values[i] === 'string')
+				string += `"${keys[i]}":"${values[i]}"`;
+			else
+				string += `"${keys[i]}":${values[i]}`;
+			if (i !== keys.length - 1)
+				string += ',';
+		}
+		return string + '}';
 	}
 }
 
@@ -116,19 +130,8 @@ function createSpotRequest(id, method, apiKey, params, time, apiSecret) {
 	return new SpotRequest(id, method, apiKey, params, time, apiSecret);
 }
 
-/**
- * Generates a digital signature of given API request
- * @param {SpotRequest} request request body to generate digital signature
- * @returns {String} digital signature for this request
- */
-function generateSignature(request) {
-	request.sig = signRequest(request, request.apiKey, apiSecret);
-	return this.sig;
-}
-
 module.exports = {
 	createSpotRequest,
-	generateSignature,
-	signRequest,
 	SpotRequest,
 };
+// TODO?: add sign request externally made request not in class??
