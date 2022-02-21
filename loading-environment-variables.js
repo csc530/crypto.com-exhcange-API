85354c5da81df4efd2b36bdf306e79bcecebcebf
csc530 <cscousin@lakeheadu.ca>
2022-02-21 11:39:40 -0500

    chore(main): :fire: Delete duplicate file

--- loading-environment-variables.js
+++
@@ -1,7 +0,0 @@
-//initilalizes vars from .env into the system's environment variables upon runtime
-//returns an object with all vars from .env
-//rename a decontructed variable
-const {parsed: envVars} = require('dotenv').config();
-console.log(`Environment variables from .env file:\n${envVars}`);
-//logs all the other environment (from the current running OS) variables
-console.log(`Local environment variables:${process.env.toString()}`);
