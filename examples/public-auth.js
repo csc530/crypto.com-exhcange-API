3f95b75a4709dbb4e6a1033de88d37f0691ab540
csc530 <cscousin@lakeheadu.ca>
2022-02-21 11:45:22 -0500

    perf(examples): :passport_control: Simplify code to only perient authorization methods
    It doesn't work I still get UNAUTHORIZED error, I suspect it's how the signature is signed :(

--- examples/public-auth.js
+++ examples/public-auth.js
@@ -1,38 +1,35 @@
 // Websocket connection
 const ws = require('ws');
 const { parsed: keys } = require('dotenv').config({ path: '../.env' });
-const req = require('express/lib/request');
-const https = require('https');
 const endpoints = require('../modules/endpoints');
 const methods = require('../modules/methods');
 const spotRequest = require('../modules/spotRequest');
-
 //create a spot request object, only to authorize the connection
-let request = spotRequest.createSpotRequest(111, methods.auth, keys.read_api, {}, new Date().getTime(), keys.read_secret);
-//genereate the digital Signature
-request.generateSignature(keys.read_secret);
-const url = request.toURL(endpoints.uat.websocket.user, methods.auth);
-// console.log(url);
+let request = spotRequest.createSpotRequest(53, methods.public.auth, keys.trade_api, {}, new Date().getTime());
 
-// To open a web socket connection
-//only enter the endpoint and everything else a parameter(?)
+/* To open a web socket connection only enter the endpoint and everything else a parameter in the messages(send)*/
 const websocket = new ws.WebSocket(endpoints.uat.websocket.user);
 websocket.on('error', err => {
-	console.error(`Error: ${err}\n`);
+    console.error(`Error: ${err}\n`);
 });
+
 websocket.on('open', () => {
-	console.log('opened websocket to -');
+    console.log('websocket opened to - ' + websocket.url);
+    //genereate the digital Signature
+    request.generateSignature(keys.read_secret);
     //when the connection is opened send (JSON) info
     //for their method and it's required parameters
-    websocket.send()
-    console.log(websocket.url);
-    console.log();
+    const send = request.toString();
+    console.log("send params = \n" + send);
+
+    websocket.send(send)
 });
-websocket.on('message', (data,isbinary) => {
-    console.log("Incoming msg:");
-    console.log(data + "\nBinary: " + isbinary+"\n");
+
+websocket.on('message', (data, isbinary) => {
+    console.log(`\nMsg:\n${data}`);
 });
-websocket.on('close', (code, reason)=>{
-    console.log("WebSocket closed");
+
+websocket.on('close', (code, reason) => {
+    console.log("\nWebSocket closed");
     console.log(`Exit code: ${code}\nReason: ${reason}`);
 });
